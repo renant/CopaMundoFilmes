a4ae605499ce7fd6d7c1ca6f9b75d65d93f5f278
Renan Teixeira <renan_replay@live.com>
2019-10-12 13:26:32 -0300

    criando estrutura do react

---
+++ Web/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Routes />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ Web/src/pages/main/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const Main = () => (
+  <Fragment>
+    <h1>Main</h1>
+  </Fragment>
+);
+
+export default Main;
